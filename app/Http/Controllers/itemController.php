0884adfda41a51fb8564d777a1cdf8c271ecfb7f
damhacker04 <adamemier16@gmail.com>
2025-12-26 07:39:19 +0700

    Implement item creation and editing functionality with validation; enhance edit view for better user experience

--- app/Http/Controllers/itemController.php
+++ app/Http/Controllers/itemController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Item;
+use App\Models\Category;
+use App\Models\OrderItem;
+use App\Models\Menu;
+use App\Models\Order;
+use Illuminate\Support\Facades\Storage;
+
 
 class itemController extends Controller
 {
@@ -32,7 +38,37 @@ class itemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+            'category_id' => 'required|exists:categories,id',
+            'img' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'is_active' => 'required|boolean',
+        ], [
+            'name.required' => 'Nama menu harus diisi.',
+            'description.string' => 'Deskripsi harus berupa teks.',
+            'price.required' => 'Harga harus diisi.',
+            'price.numeric' => 'Harga harus berupa angka.',
+            'category_id.required' => 'Kategori harus dipilih.',
+            'category_id.exists' => 'Kategori tidak ditemukan.',
+            'img.image' => 'File harus berupa gambar.',
+            'img.mimes' => 'Format gambar tidak didukung.', 
+            'img.max' => 'Batas maksimum file adalah 2 MB'
+            'is_active.required'=>'Status aktif harus diisi',
+            'is_active.boolean'=>'Status aktif harus bersifat true atau false',
+        ]);
+
+        if($request->hasFile('image')){
+            $image = $request->file('img');
+            $imageName = time().'.'.$image->getClientOriginalExtension();
+            $image->move(public_path('img_item_upload'), $imageName);
+            $validateData['img'] = $imageName;
+        }
+
+        $item = Item::create($validatedData);
+
+        return redirect()->route('items.index')->with('success', 'Item created successfully');
     }
 
     /**
@@ -40,7 +76,7 @@ class itemController extends Controller
      */
     public function show(string $id)
     {
-        //
+        
     }
 
     /**
@@ -48,7 +84,9 @@ class itemController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $item = Item::findOrFail($id);
+        $categories = Category::orderBy('cat_name', 'asc')->get();
+        return view('admin.item.edit', compact('item', 'categories'));
     }
 
     /**
@@ -56,7 +94,38 @@ class itemController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+            'category_id' => 'required|exists:categories,id',
+            'img' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'is_active' => 'required|boolean',
+        ], [
+            'name.required' => 'Nama menu harus diisi.',
+            'description.string' => 'Deskripsi harus berupa teks.',
+            'price.required' => 'Harga harus diisi.',
+            'price.numeric' => 'Harga harus berupa angka.',
+            'category_id.required' => 'Kategori harus dipilih.',
+            'category_id.exists' => 'Kategori tidak ditemukan.',
+            'img.image' => 'File harus berupa gambar.',
+            'img.mimes' => 'Format gambar tidak didukung.', 
+            'img.max' => 'Batas maksimum file adalah 2 MB'
+            'is_active.required'=>'Status aktif harus diisi',
+            'is_active.boolean'=>'Status aktif harus bersifat true atau false',
+        ]);
+
+        if($request->hasFile('img')){
+            $image = $request->file('img');
+            $imageName = time().'.'.$image->getClientOriginalExtension();
+            $image->move(public_path('img_item_upload'), $imageName);
+            $validatedData['img'] = $imageName;
+        }
+
+        $item = Item::findOrFail($id);
+        $item->update($validatedData);
+
+        return redirect()->route('items.index')->with('success', 'Item updated successfully');
     }
 
     /**
@@ -64,6 +133,8 @@ class itemController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $item = Item::findOrFail($id);
+        $item->delete();
+        return redirect()->route('items.index')->with('success', 'Item deleted successfully');
     }
 }
